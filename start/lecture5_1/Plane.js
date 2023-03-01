6df97512ba872f955c19b9fc1bcd9509b8eeafe9
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2023-03-02 01:24:21 +0530

    completed till Video1  Section 5

--- start/lecture5_1/Plane.js
+++ start/lecture5_1/Plane.js
@@ -1,53 +1,60 @@
-import { Vector3 } from '../../libs/three137/three.module.js';
-import { GLTFLoader } from '../../libs/three137/GLTFLoader.js';
+import { Vector3 } from "../../libs/three137/three.module.js";
+import { GLTFLoader } from "../../libs/three137/GLTFLoader.js";
 
-class Plane{
-    constructor(game){
-        this.assetsPath = game.assetsPath;
-        this.loadingBar = game.loadingBar;
-        this.game = game;
-        this.scene = game.scene;
-        this.load();
-        this.tmpPos = new Vector3();
-    }
+class Plane {
+  constructor(game) {
+    this.assetsPath = game.assetsPath;
+    this.loadingBar = game.loadingBar;
+    this.game = game;
+    this.scene = game.scene;
+    this.load();
+    this.tmpPos = new Vector3();
+  }
 
-    get position(){
-        if (this.plane!==undefined) this.plane.getWorldPosition(this.tmpPos);
-        return this.tmpPos;
-    }
+  get position() {
+    if (this.plane !== undefined) this.plane.getWorldPosition(this.tmpPos);
+    return this.tmpPos;
+  }
 
-    set visible(mode){
-        this.plane.visible = mode;
-    }
+  set visible(mode) {
+    this.plane.visible = mode;
+  }
 
-    load(){
-    	const loader = new GLTFLoader( ).setPath(`${this.assetsPath}plane/`);
-        this.ready = false;
-        
-		// Load a glTF resource
-		loader.load(
-			// resource URL
-			'microplane.glb',
-			// called when the resource is loaded
-			gltf => {
-                
-			},
-			// called while loading is progressing
-			xhr => {
-				
-			},
-			// called when loading has errors
-			err => {
+  load() {
+    const loader = new GLTFLoader().setPath(`${this.assetsPath}plane/`);
+    this.ready = false;
 
-				console.error( err );
+    // Load a glTF resource
+    loader.load(
+      // resource URL
+      "microplane.glb",
+      // called when the resource is loaded
+      (gltf) => {
+        this.scene.add(gltf.scene);
+        this.plane = gltf.scene;
+        this.velocity = new Vector3(0, 0, 0.1);
 
-			}
-		);
-	}	
+        this.propeller = this.plane.getObjectByName("propeller");
 
-    update(time){
-        
-    }
+        this.ready = true;
+      },
+      // called while loading is progressing
+      (xhr) => {
+        this.loadingBar.update("plane", xhr.loaded, xhr.total);
+      },
+      // called when loading has errors
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
+
+  update(time) {
+    if (this.propeller !== undefined) this.propeller.rotateZ(0.1);
+
+    this.plane.rotation.set(0, 0, Math.sin(time * 3) * 0.2, "XYZ");
+    this.plane.position.y = Math.cos(time) * 1.5;
+  }
 }
 
 export { Plane };
